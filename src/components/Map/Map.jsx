59b1221073e6fc5264d625b2ae1556ad59a1b173
Park-Yuyeong <dbduda1@kookmin.ac.kr>
2024-06-18 01:02:28 +0900

    feat: 0618 kakao map API 지도 불러오기

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { useEffect } from 'react';
 
 const Map = () => {
-  return <div>Map</div>;
+  useEffect(() => {
+    if (window.kakao && window.kakao.maps) {
+      const kakao = window.kakao;
+      const container = document.getElementById('map');
+      const options = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3
+      };
+      const map = new kakao.maps.Map(container, options);
+    }
+  }, []);
+
+  return <div id="map" style={{ width: '500px', height: '500px' }}></div>;
 };
 
 export default Map;
