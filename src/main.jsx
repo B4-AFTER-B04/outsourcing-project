e95ed7b3825c97a0d0bec109f8ab231453a2fff3
yeeunseo-dev <anfrhrl96@gmail.com>
2024-06-19 14:31:34 +0900

    feat:한번 더커밋

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <GlobalStyle />
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <GlobalStyle />
+    <App />
+  </QueryClientProvider>
 );
